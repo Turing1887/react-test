32a7498b7a5ed2bbae04b51583f433577bb1f788
Turing1887 <tim.zschage@t-online.de>
2016-10-11 09:19:50 +0200

    Update helloworld.js

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,12 +1,19 @@
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
-);
+  class HelloWorld extends React.Component {
+    render() {
+        return (
+        <p>
+            Hello, <input type="text" placeholder="Your name here" />!
+            It is {this.props.date.toTimeString()}
+        </p>
+        );
+    }
+    }
 
-class MyPanel extends Component{
-  render(){
-    
-  }
-}
+    function tick() {
+    ReactDOM.render(
+        <HelloWorld date={new Date()} />,
+        document.getElementById('example')
+    );
+    }
 
-var user;
+    setInterval(tick, 500);
